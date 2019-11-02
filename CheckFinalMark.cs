66322df275485f8f0bd8bcc5bb76a70e622187ab
Alberto Girón <alberot11@gmail.com>
2019-11-02 12:10:35 +0100

    Code is clean and comments are in place. Work done

--- CheckFinalMark.cs
+++ CheckFinalMark.cs
@@ -1,11 +1,17 @@
 using System;
+
+/*Program that asks the user to enter 10 integer, positive numbers and, at the 
+  end of the process, the program must tell how many increments and decrements 
+  have taken place.*/
+
 class CheckFinalMark
 {
     static void Main ()
     {
-        const int NUM = 10;
-        int inc = 0, dec = 0, prev = 0;
-        int [] grades = new int [NUM];
+        const int AMOUNT = 10;
+        int increment = 0, decrement = 0;
+        int [] grades = new int [AMOUNT];
+        
         for(int i=0 ; i < grades.Length ; i++)
         {
             do
@@ -13,17 +19,14 @@ class CheckFinalMark
                 Console.Write("Enter grade number {0}: ", i+1);
                 grades [i] = Convert.ToInt32(Console.ReadLine());
             } while (grades [i] < 0);
-            if(grades [i] < prev)
-            {
-                dec++;
-            }
-            else if (grades [i] > prev && i!=0)
-            {
-                inc++;
-            }
-            prev= grades [i];
+            if(i > 0 && grades [i] < grades [i-1])
+                decrement++;
+            else if (i > 0 && grades [i] > grades [i-1] && i!=0)
+                increment++;
         }
+        
         Console.Write("There has been {0} increments and {1} decrements.",
-            inc, dec);        
+            increment, decrement);        
     }
-}
+} 
+
--- TextWithoutA.cpp
+++ TextWithoutA.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
 
+/*Program that asks the user to enter a text iteratively until he/she types 
+  a text with no 'a' or 'A' symbols in it.*/
+
 int main() {
     string text;
     bool isItA;
+    
     do
     {
-        cout<< "Type any text: ";
-        cin>> text;
+        cout << "Type any text: ";
+        cin >> text;
         isItA = false;
-        for (unsigned int i=0 ; i<text.length() ; i++)
+        for (unsigned int i=0 ; i < text.length() ; i++)
         {
-            if (text [i]=='a')
+            if (text [i] == 'a')
             {
-                isItA=true;
+                isItA = true;
             }
         }
     } while (isItA == true);
 }
+
